72ca54b47400453455027d02fa052061ff1c57e9
bala-amypo <bala@amypo.in>
2025-12-17 10:04:08 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -2,32 +2,32 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
-
-import java.util.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
 
-    @Autowired
-    StudentService ser;
-    
-    @PostMapping("/add_data")
+    private final StudentService studentService;
 
-    public Student createData(@RequestBody Student stu){
-        return ser.createData(stu);
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
 
-    @GetMapping("/fetch_data")
-    public List<Student> fetchRecord(){
-        return ser.fetchRecord();
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
     }
 
-    @GetMapping("/fetch_data_id/{id}")
-    public Optional<Student> fetchDataById(@PathVariable int id){
-        return ser.fetchDataById(id);
+    @PostMapping
+    public Student saveStudent(@RequestBody Student student) {
+        return studentService.saveStudent(student);
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
     }
 }
